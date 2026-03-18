3c8c80c9bd11896d3f407adda664ecc755640116
khurram-javaid <khurram332b@gmail.com>
2026-03-18 14:05:06 +0500

    Clearing clutter
    this is my first code in which i learnt about files common js and eccma script

---
+++ index.js
@@ -0,0 +1,22 @@
+import fs from "fs/promises"
+import fsn from "fs"
+import path from "path"
+
+
+const basepath = "Basepath"
+
+let files = await fs.readdir(basepath)
+
+for(const item of files){
+    let ext = item.split(".")[item.split(".").length-1]
+    if(ext != "js" && ext!= "json"&& item.split(".").length-1){
+
+        
+        if (fsn.existsSync(path.join(basepath,ext))) {
+            fs.rename(path.join(basepath, item), path.join(basepath, ext, item))
+        }
+        else{
+            fs.mkdir(ext)
+        }
+    }
+}
